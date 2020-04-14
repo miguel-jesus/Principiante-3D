5bf7388610957f4a9819e7307917efd64a140972
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-04-14 19:13:40 +0200

    final para el juego
    repaso bastante claro y sencilla del canvas con sus anchors y del trigger en este caso ha sido solo el de enter pero aun asi han sido faciles de seguir y bastante explicativos

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public float displayImageDuration = 1f;
+
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        //en cuanto el pj entre al trigger y sea igual al player,nuestro booleano pasara a ser true
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        //cronómetro que empieza a contar desde el momento que lo cruza
+        m_Timer += Time.deltaTime;
+        //Duración del desvanecimiento y asi iremos incrementando el alpga cada vez mas porque llamaremos a esta funcion
+        //desde el update
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
